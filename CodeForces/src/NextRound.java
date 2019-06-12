a75cfbdcfbab09751a81017954aeeeb60ee8f876
StormFalcon32 <benchen9708@gmail.com>
2019-06-12 15:59:07 -0400

    Started NextRound

---
+++ CodeForces/src/NextRound.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class NextRound {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		
+		in.close();
+	}
+}
