84f4cfc9e4e855c1d6c6043d3e7443bee7a11796
StormFalcon32 <benchen9708@gmail.com>
2019-06-13 12:18:20 -0400

    Finished NextRound

--- CodeForces/src/NextRound.java
+++ CodeForces/src/NextRound.java
@@ -5,7 +5,22 @@ public class NextRound {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int N = in.nextInt();
-		
+		int K = in.nextInt();
+		int score = 1;
+		int i;
+		int numZeros = 0;
+		for (i = 0; i < N; i++) {
+			int s = in.nextInt();
+			if (i == K - 1) {
+				score = s;
+			} else if (s < score || s == 0) {
+				break;
+			}
+			if (s == 0) {
+				numZeros++;
+			}
+		}
+		System.out.println(i - numZeros);
 		in.close();
 	}
 }
