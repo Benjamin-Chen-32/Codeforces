ddc101dae8b18def486cdb0c49e7eb768cb94fa5
StormFalcon32 <benchen9708@gmail.com>
2021-09-01 18:39:36 -0500

    Added template

---
+++ Codeforces/src/templates/DefaultTemplates.java
@@ -0,0 +1,18 @@
+package templates;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DefaultTemplates {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tk = new StringTokenizer(in.readLine());
+
+		int N = Integer.parseInt(tk.nextToken());
+		System.out.println(N);
+		
+		in.close();
+	}
+}
