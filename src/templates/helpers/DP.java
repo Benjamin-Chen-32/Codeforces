e34ef778f599ff50c1ea84e7cfdacf7a8bb96be1
StormFalcon32 <benchen9708@gmail.com>
2021-10-02 17:23:53 -0500

    Merging templates here

---
+++ src/templates/helpers/DP.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class DP {
+	static int lis(int[] arr) {
+		int[] tail = new int[arr.length];
+		int length = 1;
+		tail[0] = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > tail[length - 1]) {
+				tail[length++] = arr[i];
+			} else {
+				int ind = Arrays.binarySearch(tail, 0, length - 1, arr[i]);
+				if (ind < 0) {
+					ind = -1 * ind - 1;
+				}
+				tail[ind] = arr[i];
+			}
+		}
+		return length;
+	}
+
+	public static int lcs(int[] a, int[] b) {
+		int N = a.length;
+		int M = b.length;
+		int dp[][] = new int[2][M + 1];
+		int ind = 0;
+		for (int i = 0; i <= N; i++) {
+			ind = i & 1;
+			for (int j = 0; j <= M; j++) {
+				if (i == 0 || j == 0) {
+					dp[ind][j] = 0;
+				} else if (a[i - 1] == b[j - 1]) {
+					dp[ind][j] = dp[1 - ind][j - 1] + 1;
+				} else {
+					dp[ind][j] = Math.max(dp[1 - ind][j], dp[ind][j - 1]);
+				}
+			}
+		}
+		return dp[ind][M];
+	}
+}
---
+++ src/templates/helpers/DataStructures.java
@@ -0,0 +1,146 @@
+import java.util.Arrays;
+
+public class DataStructures {
+	static class BIT {
+		int BIT[];
+		int N;
+
+		public BIT(int N) {
+			this.N = N;
+			BIT = new int[N + 1];
+		}
+
+		public int get(int index) {
+			int sum = 0;
+			index++;
+			while (index > 0) {
+				sum += BIT[index];
+				index -= index & (-index);
+			}
+			return sum;
+		}
+
+		public void update(int index, int val) {
+			index++;
+			while (index <= N) {
+				BIT[index] += val;
+				index += index & (-index);
+			}
+		}
+	}
+
+	static class DisjointSet {
+		int[] arr;
+		int[] size;
+		int N;
+
+		public DisjointSet(int N) {
+			this.N = N;
+			arr = new int[N];
+			size = new int[N];
+			Arrays.fill(size, 1);
+			for (int i = 0; i < N; i++) {
+				arr[i] = i;
+			}
+		}
+
+		public int root(int curr) {
+			while (arr[curr] != curr) {
+				arr[curr] = arr[arr[curr]];
+				curr = arr[curr];
+			}
+			return curr;
+		}
+
+		public boolean find(int a, int b) {
+			if (root(a) == root(b)) {
+				return true;
+			}
+			return false;
+		}
+
+		public void union(int a, int b) {
+			int aRoot = root(a);
+			int bRoot = root(b);
+			if (size[aRoot] < size[bRoot]) {
+				// Subset B is bigger than subset A, so B should be A's parent
+				arr[aRoot] = arr[bRoot];
+				size[bRoot] += size[aRoot];
+			} else {
+				arr[bRoot] = arr[aRoot];
+				size[aRoot] += size[bRoot];
+			}
+		}
+	}
+
+	static class SegmentTree {
+		int[] tree;
+		int[] arr;
+		int N;
+
+		public SegmentTree(int[] arr, int N) {
+			this.arr = arr;
+			this.N = N;
+			int height = (int) (Math.ceil(Math.log(N) / Math.log(2))) + 1;
+			int length = (int) Math.pow(2, height);
+			tree = new int[length];
+			build(1, 0, N - 1);
+		}
+
+		void merge(int node) {
+			tree[node] = tree[2 * node] + tree[2 * node + 1];
+		}
+
+		void build(int node, int start, int end) {
+			if (start == end) {
+				tree[node] = arr[start];
+			} else {
+				int mid = (start + end) / 2;
+				build(2 * node, start, mid);
+				build(2 * node + 1, mid + 1, end);
+				merge(node);
+			}
+		}
+
+		void update(int ind, int val) {
+			updateUtil(1, 0, N - 1, ind, val);
+		}
+
+		void updateUtil(int node, int start, int end, int ind, int val) {
+			if (start == end) {
+				arr[ind] = val;
+				tree[node] = val;
+			} else {
+				int mid = (start + end) / 2;
+				if (start <= ind && ind <= mid) {
+					updateUtil(2 * node, start, mid, ind, val);
+				} else {
+					updateUtil(2 * node + 1, mid + 1, end, ind, val);
+				}
+				merge(node);
+			}
+		}
+
+		int query(int l, int r) {
+			if (r < l) {
+				int temp = l;
+				l = r;
+				r = temp;
+			}
+			return queryUtil(1, 0, N - 1, l, r);
+		}
+
+		int queryUtil(int node, int start, int end, int l, int r) {
+			if (r < start || end < l) {
+				return 0;
+			}
+			if (l <= start && end <= r) {
+				return tree[node];
+			}
+			int mid = (start + end) / 2;
+			int p1 = queryUtil(2 * node, start, mid, l, r);
+			int p2 = queryUtil(2 * node + 1, mid + 1, end, l, r);
+			return p1 + p2;
+		}
+	}
+}
