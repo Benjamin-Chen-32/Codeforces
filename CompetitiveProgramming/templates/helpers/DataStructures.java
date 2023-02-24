919ee04fa8b6efdfd8afa110ec5b5e8c47f8e0fb
Benjamin Chen <benchen9708@gmail.com>
2023-02-24 14:49:22 -0600

    test bit, union find, segtree

--- CompetitiveProgramming/templates/helpers/DataStructures.java
+++ CompetitiveProgramming/templates/helpers/DataStructures.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 
 public class DataStructures {
-	static class BIT {
+	static class BIT { // Yosupo verified
+		// Usable for associative and invertible operations
+		// Lower const factor for memory than segtree
 		int[] BIT;
 		int N;
 
@@ -10,38 +12,41 @@ public class DataStructures {
 			BIT = new int[N + 1];
 		}
 
-		public BIT(int[] vals) {
+		public BIT(int[] vals) { // O(NlogN)
 			this(vals.length);
 			for (int i = 0; i < vals.length; i++) {
 				this.update(i, vals[i]);
 			}
 		}
 
-		public int get(int index) {
+		// Sums from 0 to index inclusive
+		// Note: summing from i to j inclusive is
+		// bit.get(j) - (i == 0 ? 0 : bit.get(i))
+		public int get(int index) { // O(logN)
 			int sum = 0;
 			index++;
 			while (index > 0) {
-				sum += BIT[index];
+				sum += BIT[index]; // Operation
 				index -= index & (-index);
 			}
 			return sum;
 		}
 
-		public void update(int index, int val) {
+		public void update(int index, int val) { // O(logN)
 			index++;
 			while (index <= N) {
-				BIT[index] += val;
+				BIT[index] += val; // Operation
 				index += index & (-index);
 			}
 		}
 	}
 
-	static class DisjointSet {
+	static class DisjointSet { // Yosupo verified
 		int[] arr;
 		int[] size;
 		int N;
 
-		public DisjointSet(int N) {
+		public DisjointSet(int N) { // O(N)
 			this.N = N;
 			arr = new int[N];
 			size = new int[N];
@@ -59,14 +64,14 @@ public class DataStructures {
 			return curr;
 		}
 
-		public boolean find(int a, int b) {
+		public boolean find(int a, int b) { // O(logN)
 			if (root(a) == root(b)) {
 				return true;
 			}
 			return false;
 		}
 
-		public void union(int a, int b) {
+		public void union(int a, int b) { // O(logN)
 			int aRoot = root(a);
 			int bRoot = root(b);
 			if (size[aRoot] < size[bRoot]) {
@@ -80,7 +85,9 @@ public class DataStructures {
 		}
 	}
 
-	static class SegTree {
+	static class SegTree { // Yosupo verified
+		// Usable for associative operations
+		// For range update queries
 
 		private int size;
 		private int[] tree;
@@ -105,18 +112,21 @@ public class DataStructures {
 
 		private void build() {
 			for (int i = 2 * size - 1; i > 1; i -= 2) {
-				tree[i >> 1] = tree[i] + tree[i ^ 1];
+				tree[i >> 1] = tree[i] + tree[i ^ 1]; // Operation
 				sz[i >> 1] = sz[i] + sz[i ^ 1];
 			}
 		}
 
-		public void update(int lo, int hi, int val) {
+		// Update range from lo to hi inclusive
+		public void update(int lo, int hi, int val) { // O(logN)
 			for (int l = lo + size, r = hi + size; l <= r; l = (l + 1) >> 1, r = (r - 1) >> 1) {
 				if ((l & 1) > 0) {
+					// Operation
 					tree[l] += val * sz[l];
 					lazy[l] += val;
 				}
 				if ((r & 1) == 0) {
+					// Operation
 					tree[r] += val * sz[r];
 					lazy[r] += val;
 				}
@@ -127,18 +137,20 @@ public class DataStructures {
 
 		private void pushUp(int i) {
 			for (; i > 1; i >>= 1) {
+				// Operation
 				tree[i >> 1] = tree[i] + tree[i ^ 1] + lazy[i >> 1] * sz[i >> 1];
 			}
 		}
 
-		public int query(int lo, int hi) {
+		// Query from lo to hi inclusive
+		public int query(int lo, int hi) { // O(logN)
 			int sum = 0;
 			for (lo += size, hi += size; lo <= hi; lo = (lo + 1) >> 1, hi = (hi - 1) >> 1) {
 				if ((lo & 1) > 0) {
-					sum += getValue(lo);
+					sum += getValue(lo); // Operation
 				}
 				if ((hi & 1) == 0) {
-					sum += getValue(hi);
+					sum += getValue(hi); // Operation
 				}
 			}
 			return sum;
@@ -147,7 +159,7 @@ public class DataStructures {
 		public int getValue(int i) {
 			int res = tree[i];
 			for (int j = i >> 1; j > 0; j >>= 1) {
-				res += lazy[j] * sz[i];
+				res += lazy[j] * sz[i]; // Operation
 			}
 			return res;
 		}
