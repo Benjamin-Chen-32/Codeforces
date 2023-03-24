e69bc020c7ed142ea12ff99ff0922a7c67638861
Benjamin Chen <benchen9708@gmail.com>
2023-03-24 18:17:12 -0500

    fix template

--- CompetitiveProgramming/templates/Templates.java
+++ CompetitiveProgramming/templates/Templates.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class Templates {
+public class CalibrationComplications {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
