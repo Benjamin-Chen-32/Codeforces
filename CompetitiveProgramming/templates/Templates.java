f2052e07111e834e41968a3fdd8bede42561c576
Benjamin Chen <benchen9708@gmail.com>
2023-03-24 19:17:17 -0500

    whoops

--- CompetitiveProgramming/templates/Templates.java
+++ CompetitiveProgramming/templates/Templates.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class CalibrationComplications {
+public class Templates {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
